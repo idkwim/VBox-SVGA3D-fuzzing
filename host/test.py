057e5322021a3e8edb9bca3a436d1088305dbfba
candymate <ys3f4@naver.com>
2020-12-04 19:20:57 +0900

    Added a working crash test case (issue-3)

--- host/test.py
+++ host/test.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 
+import ctypes
+
+import svga_reg
+import svga3d_reg
+
 import generator
 import printer
 
@@ -17,6 +22,48 @@ def test_print_cmd():
     print("TESTING CMD " + str(i))
     cmd = printer.print_cmd(generator.gen_cmd(i))
 
+def gen_issue_3():
+  cmd = b""
+  cmd += generator.p32(1040) # SVGA_3D_CMD_SURFACE_DEFINE
+  cmd += generator.p32(48) # header size
+
+  surfaceFaces = (svga3d_reg.SVGA3dSurfaceFace*6)( \
+    svga3d_reg.SVGA3dSurfaceFace(1), \
+    svga3d_reg.SVGA3dSurfaceFace(0), \
+    svga3d_reg.SVGA3dSurfaceFace(0), \
+    svga3d_reg.SVGA3dSurfaceFace(0), \
+    svga3d_reg.SVGA3dSurfaceFace(0), \
+    svga3d_reg.SVGA3dSurfaceFace(0) \
+  )
+  cmd += bytes(svga3d_reg.SVGA3dCmdDefineSurface(0, 2, 37, surfaceFaces))
+  cmd += bytes(svga3d_reg.SVGA3dSize(-1, -1, -1))
+
+  cmd += generator.p32(1040) # SVGA_3D_CMD_SURFACE_DEFINE
+  cmd += generator.p32(48) # header size
+  cmd += bytes(svga3d_reg.SVGA3dCmdDefineSurface(1, 2, 37, surfaceFaces))
+  cmd += bytes(svga3d_reg.SVGA3dSize(-1, -1, -1))
+
+  cmd += generator.p32(1043) # SVGA_3D_CMD_SURFACE_STRETCHBLT
+  cmd += generator.p32(ctypes.sizeof(svga3d_reg.SVGA3dCmdSurfaceStretchBlt)) # header size
+  cmd += bytes(svga3d_reg.SVGA3dCmdSurfaceStretchBlt( \
+    svga3d_reg.SVGA3dSurfaceImageId(0, 0, 0), \
+    svga3d_reg.SVGA3dSurfaceImageId(1, 0, 0), \
+    svga3d_reg.SVGA3dBox(0, 0, 0, 1, 1, 1), \
+    svga3d_reg.SVGA3dBox(0, 0, 0, 1, 1, 1), \
+    0 \
+  ))
+
+  cmd += generator.p32(1044) # SVGA_3D_CMD_SURFACE_DMA
+  cmd += generator.p32(64) # header size
+  cmd += bytes(svga3d_reg.SVGA3dCmdSurfaceDMA( \
+    svga3d_reg.SVGA3dGuestImage(svga3d_reg.struct_SVGAGuestPtr(-2, 0), 0), \
+    svga3d_reg.SVGA3dSurfaceImageId(0, 0, 0), \
+    2 \
+  ))
+  cmd += bytes(svga3d_reg.SVGA3dCopyBox(0, 0, 0, 1, 1, 1, 0, 0, 0))
+  
+  return cmd
+
 if __name__ == "__main__":
   # test codes
 
@@ -24,4 +71,11 @@ if __name__ == "__main__":
   # test_cmd_error()
 
   # printer test codes
-  test_print_cmd()
+  # test_print_cmd()
+
+  # issue-3
+  raw = gen_issue_3()
+  with open("issue-3.pkts", "wb") as f:
+    f.write(raw)
+  while len(raw) > 0:
+    raw = printer.print_cmd(raw)
